91bd5e330da390eb09ff6043678d9b6c835a9b14
carantoin <90127329+carantoin@users.noreply.github.com>
2024-10-05 18:42:28 +0200

    Initial commit

---
+++ Lesson2/Program.cs
@@ -0,0 +1,13 @@
+﻿try
+{
+    Console.WriteLine("Введите R");
+    double R = double.Parse(Console.ReadLine());
+    double L = 2 * Double.Pi * R;
+    Console.WriteLine($"L={L:F2}");
+    double S = Double.Pi * R * R;
+    Console.WriteLine($"S={S:F2}");
+}
+catch
+{
+    Console.WriteLine("Введите правильные данные!");
+}
